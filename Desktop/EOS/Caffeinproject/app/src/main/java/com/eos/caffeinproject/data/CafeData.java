c2cd413b9e0c956cb0eebb29895922d2b5ed7590
2019088277 <2019088277@hanyang.ac.kr>
2021-07-03 02:19:42 +0900

    202babo~~

---
+++ Desktop/EOS/Caffeinproject/app/src/main/java/com/eos/caffeinproject/data/CafeData.java
@@ -0,0 +1,10 @@
+package com.eos.caffeinproject.data;
+
+import java.util.Date;
+
+public class CafeData {
+    public int id;
+    public String cafeName;
+    public int status;
+    public String time;
+}
