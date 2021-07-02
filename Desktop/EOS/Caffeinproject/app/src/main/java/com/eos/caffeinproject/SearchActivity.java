708c215aafc6c7ebd6d07fdb6845793e0d59fd79
kimeeyoung <iamagirl99@naver.com>
2021-07-02 20:38:21 +0900

    지도추가

--- Desktop/EOS/Caffeinproject/app/src/main/java/com/eos/caffeinproject/SearchActivity.java
+++ Desktop/EOS/Caffeinproject/app/src/main/java/com/eos/caffeinproject/SearchActivity.java
@@ -10,15 +10,53 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class SearchActivity extends Fragment {
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class SearchActivity extends Fragment implements OnMapReadyCallback {
     View v;
+    private GoogleMap mMap;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         v = inflater.inflate(R.layout.activity_search,container, false);
+  //      SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+   //             .findFragmentById(R.id.map);
+  //      mapFragment.getMapAsync(this);
 
         return v;
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+
+
+        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
+
+        // CameraUpdateFactory.zoomTo가 오동작하네요.
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+
+
     }
 
 }
