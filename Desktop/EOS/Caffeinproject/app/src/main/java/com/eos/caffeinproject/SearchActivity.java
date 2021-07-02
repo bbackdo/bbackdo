18bb1c42aebb3d39769d8fa9385248cc6fa556bf
kimeeyoung <iamagirl99@naver.com>
2021-07-03 02:51:30 +0900

    지도 마크 추가

--- Desktop/EOS/Caffeinproject/app/src/main/java/com/eos/caffeinproject/SearchActivity.java
+++ Desktop/EOS/Caffeinproject/app/src/main/java/com/eos/caffeinproject/SearchActivity.java
@@ -101,16 +101,75 @@ public class SearchActivity extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
-        LatLng SEOUL = new LatLng(37.59039640884452, 126.94058289771243);
+        LatLng SEOUL = new LatLng(37.559970, 127.042217);
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("202집..");
-        googleMap.addMarker(markerOptions);
+//        MarkerOptions markerOptions1 = new MarkerOptions();
+//        markerOptions1.position(SEOUL);
+//        markerOptions1.title("202집");
+//        markerOptions1.snippet("웰컴...");
+//        googleMap.addMarker(markerOptions1);
         //googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
         //googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(SEOUL);
+        markerOptions2.title("보경이집");
+        markerOptions2.snippet("홈스윗홈...");
+        googleMap.addMarker(markerOptions2);
+
+        MarkerOptions markerOptions3 = new MarkerOptions();
+        markerOptions3.position(new LatLng(37.556608, 127.045892));
+        markerOptions3.title("티아모");
+        markerOptions3.snippet("개높아...");
+        googleMap.addMarker(markerOptions3);
+
+
+        MarkerOptions markerOptions4 = new MarkerOptions();
+        markerOptions4.position(new LatLng(37.557325600092895, 127.04212326887637));
+        markerOptions4.title("스타벅스 한양대점");
+        markerOptions4.snippet("사람개많아");
+        googleMap.addMarker(markerOptions4);
+
+        MarkerOptions markerOptions5 = new MarkerOptions();
+        markerOptions5.position(new LatLng(37.55729911717727, 127.04176722695227));
+        markerOptions5.title("탐앤탐스 한양대점");
+        markerOptions5.snippet("아아개노맛");
+        googleMap.addMarker(markerOptions5);
+
+        MarkerOptions markerOptions6 = new MarkerOptions();
+        markerOptions6.position(new LatLng(37.557361535473305, 127.04163576424162));
+        markerOptions6.title("흥신소");
+        markerOptions6.snippet("갬성충");
+        googleMap.addMarker(markerOptions6);
+
+        MarkerOptions markerOptions7 = new MarkerOptions();
+        markerOptions7.position(new LatLng(37.561390317437834, 127.0383793136441));
+        markerOptions7.title("투썸플레이스 왕십리역점");
+        markerOptions7.snippet("티라미수존맛");
+        googleMap.addMarker(markerOptions7);
+
+        MarkerOptions markerOptions8 = new MarkerOptions();
+        markerOptions8.position(new LatLng(37.56038654925872, 127.04099547113613));
+        markerOptions8.title("쥬씨 한양대점");
+        markerOptions8.snippet("가성비갑1");
+        googleMap.addMarker(markerOptions8);
+
+        MarkerOptions markerOptions9 = new MarkerOptions();
+        markerOptions9.position(new LatLng(37.55988027206512, 127.04126523493424));
+        markerOptions9.title("더벤티");
+        markerOptions9.snippet("가성비갑2");
+        googleMap.addMarker(markerOptions9);
+
+        MarkerOptions markerOptions10 = new MarkerOptions();
+        markerOptions10.position(new LatLng(37.55994757838761, 127.04085248421728));
+        markerOptions10.title("빽다방 한양대점");
+        markerOptions10.snippet("가성비갑3");
+        googleMap.addMarker(markerOptions10);
+
+
+
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 17));
     }
 
 }
