f6805fbcf3541bd5ac732e4b420b1bd9d85125bc
Duck-E0730 <81903411+Duck-E0730@users.noreply.github.com>
2021-08-04 10:28:20 +0900

    윷던지기

--- app/src/main/java/com/example/bbackdo/AniActivity.kt
+++ app/src/main/java/com/example/bbackdo/AniActivity.kt
@@ -1,8 +1,10 @@
 package com.example.bbackdo
 
 import android.app.Activity
+import java.util.Random
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.bbackdo.databinding.ActivityAniBinding
 
 class AniActivity : AppCompatActivity() {
@@ -10,7 +12,7 @@ class AniActivity : AppCompatActivity() {
         ActivityAniBinding.inflate(layoutInflater)
     }
 
-    var intYut = arrayListOf<Int>(R.drawable.,R.drawable.)
+    var imgYut = arrayListOf<Int>(R.drawable.yut_0,R.drawable.yut_1)
     var Yut = arrayListOf<String>("윷","걸","개","도","모")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +20,23 @@ class AniActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ani)
 
         with(bind){
-            .setImageResource()
+            rollAniButton.setOnClickListener {
+                val random = Random()
+                val num = random.nextInt(10)
+                val n1 = 1-num/6
+                val n2 = 1-num/6
+                val n3 = 1-num/6
+                val n4 = 1-num/6
+                val n = n1 + n2 + n3 + n4
 
+                imageView1.setImageResource(imgYut[n1])
+                imageView2.setImageResource(imgYut[n2])
+                imageView3.setImageResource(imgYut[n3])
+                imageView4.setImageResource(imgYut[n4])
+
+                yutEditText.setText(Yut[n])
+
+            }
         }
-
     }
 }
