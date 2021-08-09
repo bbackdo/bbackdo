3889943724660d281a98f92fca893c71a4f0e25a
sol0617 <kimeunsol55@gmail.com>
2021-08-09 14:00:12 +0900

    방목록 디자인 수정

--- app/src/main/java/com/example/bbackdo/RoomListActivity.kt
+++ app/src/main/java/com/example/bbackdo/RoomListActivity.kt
@@ -1,17 +1,20 @@
 package com.example.bbackdo
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 //알림, 액티비티
-//동그라미를 누르면... 할 수 있게
 //업을 떄/잡을 떄/졌을 때/자리
 
 class RoomListActivity : AppCompatActivity() {
@@ -29,6 +32,17 @@ class RoomListActivity : AppCompatActivity() {
         //recyclerview.layoutManager = LinearLayoutManager(this)
 
         val buttonMake = findViewById<Button>(R.id.buttonMake)
+        val editSearchBar = findViewById<EditText>(R.id.editSearchBar)
+        val recyclerRoom = findViewById<RecyclerView>(R.id.recyclerRoom)
+
+        if(editSearchBar.text.isNotEmpty()) {
+            recyclerRoom.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+            recyclerRoom.setHasFixedSize(true)
+
+            //키보드 내리기
+            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(editSearchBar.windowToken, 0)
+        }
 
         buttonMake.setOnClickListener {
 
@@ -55,7 +69,6 @@ class RoomListActivity : AppCompatActivity() {
         inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
             val textTitle = findViewById<TextView>(R.id.textTitle)
-            val buttonEnter = findViewById<Button>(R.id.buttonEnter)
 
             init {
 
