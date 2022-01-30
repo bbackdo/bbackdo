d14c006c0276ce28a2b7e0393b08dd0d5e176e1d
Duck-E0730 <81903411+Duck-E0730@users.noreply.github.com>
2022-01-30 21:09:45 +0900

    team change

--- app/src/main/java/com/example/bbackdo/TeamAdapter.kt
+++ app/src/main/java/com/example/bbackdo/TeamAdapter.kt
@@ -5,7 +5,6 @@ import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bbackdo.TeamActivity.Extras.room
 import com.example.bbackdo.databinding.CellMemberBinding
@@ -20,7 +19,9 @@ import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ServerValue
+import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import splitties.activities.start
 import splitties.bundle.putExtras
 
@@ -48,7 +49,6 @@ class TeamAdapter(
         return listData.size
     }
 
-
     inner class ViewHolder(private val bind: LayoutTeamBinding) :
         RecyclerView.ViewHolder(bind.root) {
         fun binding(team: Team) {
@@ -58,58 +58,75 @@ class TeamAdapter(
 
                 //중첩 adapter
                 var dataList = arrayListOf<User>()
-                var memberList = HashMap<String, Any>()
+                var memberList = arrayListOf<String>()
                 Database.getReference("teams/${team.tid}/members").get().addOnSuccessListener {
-                    val membertest = it.value
-                    if (it.getValue() != null){
-                        Log.d("hhhhhrang", it.getValue().toString())
-                        memberList =  it.getValue<HashMap<String, Any>>()!!
-
-                    }
-
-                    val pastTid = team.tid
+                    val membertest = it.value.toString()
+                    memberList.add(it.value.toString())
+                    var pastTid = team.tid
                     val updates = hashMapOf(
                         "teams/${team.tid}/members/$uid" to false,
                         "users/$uid/teams/${team.tid}" to false
 
                     )
-                    //Toast.makeText(context, "${teamName.text} ${memberList}", Toast.LENGTH_SHORT).show()
 
                     Log.d("test",team.tid.toString())
-                    """
-                          teamName.setOnClickListener{
+                    teamName.setOnClickListener{
                         memberList.add(membertest)
-                        Database.getReference("").updateChildren(updates as Map<String, Any>).addOnSuccessListener {  }
+//                        Database.getReference("").updateChildren(updates as Map<String, Any>).addOnSuccessListener {  }
                         //기존 팀에서 삭제
+                        Database.getReference("users/$uid/teams").child("teams").setValue(team.tid)
+                        Database.getReference("teams/${team.tid}/members").child("members").setValue(uid)
+
+                        Database.getReference("teams/${team.tid}/members").child("members").removeValue()
                         Log.d("test",team.tid.toString())
-                    }
-                        
-                    """.trimIndent()
-
-
-
-
-                    Database.getReference("users").get().addOnSuccessListener { users ->
-                        users.children.forEach {
-                            //Toast.makeText(context, "$memberList", Toast.LENGTH_SHORT).show()
-                            //Toast.makeText(context, "${it.key}", Toast.LENGTH_SHORT).show()
-                            if (it.key in memberList.keys){
-                                dataList.add(it.getValue<User>()!!)
-                            }
-                            //Toast.makeText(context, "${dataList}", Toast.LENGTH_SHORT).show()
-//                        Log.d("memberList", memberList.toString())
-                            //if ("{".plus(it.key).plus("=false}") in memberList) {
-//                            Log.d("추가된 유저", it.getValue<User>().toString())
-                            //  dataList.add(it.getValue<User>()!!)
-                            //}
-                            val adapter = EachTeamAdapter(context, dataList)
-
-                            teamRcv.adapter = adapter
-                        }
+                        Log.d("test",pastTid.toString())
                     }
                 }
 
 
+                Database.getReference("teams/${team.tid}/memebers").addChildEventListener(object:
+                    ChildEventListener {
+                    override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                        //Toast.makeText(context, snapshot.value.toString(), Toast.LENGTH_SHORT).show()
+                        memberList.add(snapshot.value.toString())
+                        Database.getReference("users/$uid/teams/${team.tid}").removeValue()
+                        Database.getReference("teams/${team.tid}/members/$uid").removeValue()
+                    }
+
+                    override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                        Database.getReference("users/$uid/teams/${team.tid}").removeValue()
+                        Database.getReference("teams/${team.tid}/members/$uid").removeValue()
+                    }
+
+                    override fun onChildRemoved(snapshot: DataSnapshot) {
+                        Database.getReference("users/$uid/teams/${team.tid}").removeValue()
+                        memberList.remove(snapshot.value.toString())
+                        Database.getReference("rooms/$room.rid/members/$uid").removeValue()
+
+                    }
+
+                    override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+
+                Database.getReference("users").get().addOnSuccessListener { users ->
+                    users.children.forEach {
+//                        Log.d("memberList", memberList.toString())
+                        if ("{".plus(it.key).plus("=false}") in memberList) {
+//                            Log.d("추가된 유저", it.getValue<User>().toString())
+                            dataList.add(it.getValue<User>()!!)
+                        }
+                        val adapter = EachTeamAdapter(context, dataList)
+
+                        teamRcv.adapter = adapter
+                    }
+                }
             }
 
 
@@ -167,5 +184,6 @@ class EachTeamAdapter(
 
     }
 }
+
 
 
